fc7f1328270c5fc75a6c55f86818bea633cd9cbc
ganeshtechit <ganeshmoorthyit96@gmail.com>
2022-11-07 23:42:52 +0530

    js

---
+++ 01Basics/hellowold.js
@@ -0,0 +1,9 @@
+/* function sum(x, y) {
+
+    return x + y;
+}
+
+console.log(sum()); */
+
+
+console.log("Hello World!");
---
+++ 01Basics/variables.js
@@ -0,0 +1,18 @@
+let name = "ganeshtechit"
+
+let score = "150.25"
+let bonus = "550"
+
+let totalScore = score + bonus;
+
+console.log(totalScore);
+
+let firstName = "ganesh"
+firstName = "moorthi"
+
+let lastName = "techit"
+
+let myfullName = firstName + '**' + lastName;
+
+console.log(firstName + '_' + lastName);
+console.log(myfullName);
