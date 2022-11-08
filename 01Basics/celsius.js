4c994b278b1e9261dfc1f16eaad7ae929fcbb757
ganeshtechit <ganeshmoorthyit96@gmail.com>
2022-11-09 01:08:11 +0530

    nullBollean

--- 01Basics/celsius.js
+++ 01Basics/celsius.js
@@ -11,6 +11,18 @@ var complex = (((4 + 4) * 1) / (5 - 8)) //best wayto doing this
 var complex1 = ((4 + 4) + 0) - (5 + 1)
 
 
-console.log (finalScore);
-console.log (complex);
-console.log (complex1);
+// console.log (finalScore);
+// console.log (complex);
+// console.log (complex1);
+
+
+/* Temperature Calculator - Fahrenheit to Celsius*/
+
+let tempInFahreheit = 100
+
+// do calculation
+//(T(°F) - 32) × 5/9
+
+let celsius = (tempInFahreheit - 32) * 5 / 9
+
+console.log(celsius);
---
+++ 01Basics/nullandbool.js
@@ -0,0 +1,31 @@
+//temp is coming from website API
+
+// var temp = 0 // Output = 0
+
+var temp  // Output = undefined
+
+//done some calculation 
+
+// temp = 2 // Output = 2
+
+// console.log('Current temperature is: ' + temp);
+
+
+// Grade System
+
+// 10 - Great job
+// 5 - Work harder
+// 2 - poor
+// 0 - fail
+
+// Boolean - True/False
+
+let actualMarks = 40 //
+
+// let myGrade = (actualMarks < 50)
+// let myGrade = (actualMarks > 50)
+// let myGrade = (actualMarks <= 50)
+// let myGrade = (actualMarks >= 50)
+let myGrade = (actualMarks == 50)
+
+console.log(myGrade)
