9614e3d40d00e531fbce598051e2c3e2cdab9a23
ganeshtechit <ganeshmoorthyit96@gmail.com>
2022-11-09 00:18:53 +0530

    complex numbers

--- 01Basics/celsius.js
+++ 01Basics/celsius.js
@@ -3,6 +3,14 @@ var score = 100
 score = 130
 
 const bonus = 50
-var finalScore = score + bonus
+var finalScore = score + bonus // add two numbers
+
+// var complex = 4 + 4 * 1 / 5 - 8 // Not good practices
+
+var complex = (((4 + 4) * 1) / (5 - 8)) //best wayto doing this 
+var complex1 = ((4 + 4) + 0) - (5 + 1)
+
 
 console.log (finalScore);
+console.log (complex);
+console.log (complex1);
